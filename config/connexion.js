567acf64dad54930d8202780e9f01df20b227d7e
Christ Mbuli <121105340+ChristMbuli@users.noreply.github.com>
2024-04-24 05:00:31 +0100

    Update connexion.js

--- config/connexion.js
+++ config/connexion.js
@@ -1,10 +1,10 @@
 const mysql = require("mysql");
 
 const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "services",
+  host: "b99n3bpodbzuzb2y1dwp-mysql.services.clever-cloud.com",
+  user: "ukafny7tmbob0kpv",
+  password: "UDMydmpbyyvWbDWRRHIg",
+  database: "b99n3bpodbzuzb2y1dwp",
 });
 
 db.connect((err) => {
